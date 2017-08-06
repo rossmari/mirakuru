bf89e7c0f566ffed27665c6a03333b58b43de29b
rossmari <roman-bujenko@yandex.ru>
2017-08-06 18:03:06 +0700

    revert gnomes usage

--- config/initializers/chudo_bot.rb
+++ config/initializers/chudo_bot.rb
@@ -1,20 +1,20 @@
 require 'net/http'
 require 'telegram/bot'
 
-# token = Gnome.read('telegram_token')
-#
-# $bot = Telegram::Bot::Client.new(token)
-#
-# Thread.new do
-#   Telegram::Bot::Client.run(token) do |bot|
-#     bot.listen do |message|
-#       case message
-#         when Telegram::Bot::Types::CallbackQuery
-#           # Here you can handle your callbacks from inline buttons
-#           Telegram::Callbacks::Processor.perform(message)
-#         when Telegram::Bot::Types::Message
-#           Telegram::Messages::Processor.perform(message)
-#       end
-#     end
-#   end
-# end
+token = Gnome.read('telegram_token')
+
+$bot = Telegram::Bot::Client.new(token)
+
+Thread.new do
+  Telegram::Bot::Client.run(token) do |bot|
+    bot.listen do |message|
+      case message
+        when Telegram::Bot::Types::CallbackQuery
+          # Here you can handle your callbacks from inline buttons
+          Telegram::Callbacks::Processor.perform(message)
+        when Telegram::Bot::Types::Message
+          Telegram::Messages::Processor.perform(message)
+      end
+    end
+  end
+end
